e4da4e004b3785748957013c369585860363ec3c
gautaz <gautaz@users.noreply.github.com>
2018-04-25 15:09:41 +0200

    fix asynchronous tests issues

--- test/helpers-observable.js
+++ test/helpers-observable.js
@@ -12,45 +12,54 @@ test(t => {
     return observer.complete();
   });
   t.plan(2);
-  return observable.subscribe({
+  observable.subscribe({
     next: v => t.is(v, 1),
     error: () => t.fail(),
     complete: () => t.pass()
   });
+  return observable;
 });
 
 test(t => {
+  const observable = Observable.of(1);
   t.plan(2);
-  return Observable.of(1).subscribe({
+  observable.subscribe({
     next: v => t.is(v, 1),
     error: () => t.fail(),
     complete: () => t.pass()
   });
+  return observable;
 });
 
 test(t => {
+  const observable = Observable.of(1);
   t.plan(2);
-  return Observable.of(1).subscribe(
+  observable.subscribe(
     v => t.is(v, 1),
     () => t.fail(),
     () => t.pass()
   );
+  return observable;
 });
 
 test(t => {
+  const observable = Observable.from([1]);
   t.plan(2);
-  return Observable.from([1]).subscribe(
+  observable.subscribe(
     v => t.is(v, 1),
     () => t.fail(),
     () => t.pass()
   );
+  return observable;
 });
 
 test(t => {
+  const observable = Observable.from(Observable.of(1));
   t.plan(2);
-  return Observable.from(Observable.of(1)).subscribe(
+  observable.subscribe(
     v => t.is(v, 1),
     () => t.fail(),
     () => t.pass()
   );
+  return observable;
 });
